6400fdda46bcf58a6afbac971a76be6839936967
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-30 16:11:15 -0300

    Adicionando método para realizar o cadastro do aluno pelo usuario

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -15,15 +15,33 @@ public class Sistema {
                 "Digite 2 para gerênciar os alunos.\n" +
                 "Digite 3 para gerênciar a frota de veículos.");
     }
+
+
+    public static Aluno cadastrarAluno(){
+        String nome = capturarDados("Digite o nome do Aluno: \n").nextLine();
+        String cpf = capturarDados("Digite o CPF do Aluno: \n").nextLine();
+        int idade = capturarDados("Digite a idade do Aluno: \n").nextInt();
+        String telefone = capturarDados("Digite o telefone do Aluno: \n").nextLine();
+        String endereco = capturarDados("Digite o endereço do Aluno: \n").nextLine();
+        String categoria = capturarDados("Digite a categoria desejada do Aluno: \n").nextLine();
+
+        Aluno aluno = new Aluno(nome,cpf,idade,telefone,endereco,categoria);
+
+        return aluno;
+
+    }
+
+
     public static void executar() {
 
         AutoEscola autoEscola = new AutoEscola("Pé No Freio", "Esquina da 25 Março", "40028922");
         boolean loop = true;
-        Scanner scan = new Scanner(System.in);
+
 
         while (loop) {
+            menu();
+            int opcao = capturarDados("Digite a opção desejada: \n").nextInt();
 
-            int opcao = scan.nextInt();
             if (opcao == 1){
 
             }else if(opcao == 2){
