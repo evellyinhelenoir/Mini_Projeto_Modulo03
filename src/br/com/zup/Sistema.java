cd690dac0a5d93b14892d3e7b9e878f058bfe1d4
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-09-30 14:45:16 -0300

    Inicializando a classe Sistema e adicionando menu

---
+++ src/br/com/zup/Sistema.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Sistema {
+    private static void menu(){
+        System.out.println("Bem vinde à nossa auto escola\n" +
+                "Digite 1 para gerênciar os funcionários.\n" +
+                "Digite 2 para gerênciar os alunos.\n" +
+                "Digite 3 para gerênciar a frota de veículos.\n");
+    }
+}
