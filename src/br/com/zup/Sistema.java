45a1db47fff36023d38ba77c45bcc5c0237e745e
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-09-30 15:04:39 -0300

    Adicionando novo sistema de captura de dados

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -9,4 +9,10 @@ public class Sistema {
                 "Digite 2 para gerênciar os alunos.\n" +
                 "Digite 3 para gerênciar a frota de veículos.\n");
     }
+    private static Scanner capturarDados (String menssagem) {
+        System.out.println(menssagem);
+        return new Scanner(System.in);
+
+    }
+
 }
