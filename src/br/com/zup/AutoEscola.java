eae7e84a0a6d7fdbbb5f7a1f8004ebeecb18afe6
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-30 14:24:06 -0300

    Modelagem da Classe auto escola

--- src/br/com/zup/AutoEscola.java
+++ src/br/com/zup/AutoEscola.java
@@ -3,5 +3,54 @@ package br.com.zup;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AutoEscola {
+public class AutoEscola {    private String nomeDaEmpresa;
+    private String enderecoDaEmpresa;
+    private String cnpj;
+
+    private List<Funcionario> funcionario= new ArrayList<>();
+    private List<Aluno> aluno = new ArrayList<>();
+    private List<Veiculo> veiculo = new ArrayList<>();
+
+    public AutoEscola() {
+    }
+
+    public AutoEscola(String nomeDaEmpresa, String enderecoDaEmpresa, String cnpj) {
+        this.nomeDaEmpresa = nomeDaEmpresa;
+        this.enderecoDaEmpresa = enderecoDaEmpresa;
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeDaEmpresa() {
+        return nomeDaEmpresa;
+    }
+
+    public void setNomeDaEmpresa(String nomeDaEmpresa) {
+        this.nomeDaEmpresa = nomeDaEmpresa;
+    }
+
+    public String getEnderecoDaEmpresa() {
+        return enderecoDaEmpresa;
+    }
+
+    public void setEnderecoDaEmpresa(String enderecoDaEmpresa) {
+        this.enderecoDaEmpresa = enderecoDaEmpresa;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Nome da Empresa: " + nomeDaEmpresa);
+        retorno.append("\n Endereço da empresa: " + enderecoDaEmpresa);
+        retorno.append("\n CNPJ da empresa: " + cnpj);
+        return retorno.toString();
+    }
+}
 
