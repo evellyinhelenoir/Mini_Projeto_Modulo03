f221ce29e63d82b9193c782dea7e435eb86d89ce
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-09-30 15:48:38 -0300

    Adicionando esqueleto do menú para modificações

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -3,16 +3,34 @@ package br.com.zup;
 import java.util.Scanner;
 
 public class Sistema {
+
+    private static Scanner capturarDados (String menssagem) {
+        System.out.println(menssagem);
+        return new Scanner(System.in);
+    }
+
     private static void menu(){
         System.out.println("Bem vinde à nossa auto escola\n" +
                 "Digite 1 para gerênciar os funcionários.\n" +
                 "Digite 2 para gerênciar os alunos.\n" +
-                "Digite 3 para gerênciar a frota de veículos.\n");
+                "Digite 3 para gerênciar a frota de veículos.");
     }
-    private static Scanner capturarDados (String menssagem) {
-        System.out.println(menssagem);
-        return new Scanner(System.in);
+    public static void executar() {
 
+        AutoEscola autoEscola = new AutoEscola("Pé No Freio", "Esquina da 25 Março", "40028922");
+        boolean loop = true;
+        Scanner scan = new Scanner(System.in);
+
+        while (loop) {
+
+            int opcao = scan.nextInt();
+            if (opcao == 1){
+
+            }else if(opcao == 2){
+
+            }else if(opcao == 3){
+
+            }
+        }
     }
-
 }
