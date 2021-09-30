b99e26c18d108c4f1e0994f2878c0527d6f124de
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-30 12:02:46 -0300

    Modelagem da classe Pessoa

---
+++ src/br/com/zup/Pessoa.java
@@ -0,0 +1,64 @@
+package br.com.zup;
+
+public class Pessoa {
+
+    private String nomePessoa;
+    private String cpfPessoa;
+    private int idade;
+    private String telefone;
+    private String endereco;
+
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nomePessoa, String cpfPessoa, int idade, String telefone, String endereco) {
+        this.nomePessoa = nomePessoa;
+        this.cpfPessoa = cpfPessoa;
+        this.idade = idade;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public void setNomePessoa(String nomePessoa) {
+        this.nomePessoa = nomePessoa;
+    }
+
+    public String getCpfPessoa() {
+        return cpfPessoa;
+    }
+
+    public void setCpfPessoa(String cpfPessoa) {
+        this.cpfPessoa = cpfPessoa;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
